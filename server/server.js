ac64aa1e1a717f7714e2cb8ba4ae3851fcfd13fc
jeonlyung <bindongx2@naver.com>
2024-10-17 16:57:43 +0900

    react lib 버전 up

--- server/server.js
+++ server/server.js
@@ -9,10 +9,11 @@ const PORT = process.env.PORT || 5000;
 //Mongo DB 설정
 const config = require("./config/key");
 const mongoose = require('mongoose');
-const connect = mongoose.connect(config.mongoURI,
-  {
+const connect = mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, 
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true, 
+    useFindAndModify: false
   })
   .then(() => console.log('MongoDB Connected...!!'))
   .catch(err => console.log(err));
