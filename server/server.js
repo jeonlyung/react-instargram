38cb1889130edaf50c34bee6f833203807e266bc
jeonlyung <bindongx2@naver.com>
2024-10-17 15:32:37 +0900

    mongo DB 설정 . 및스키마 생성

--- server/server.js
+++ server/server.js
@@ -6,9 +6,16 @@ const cors = require('cors');
 //PORT 설정
 const PORT = process.env.PORT || 5000;
 
-
 //Mongo DB 설정
+const config = require("./config/key");
 const mongoose = require('mongoose');
+const connect = mongoose.connect(config.mongoURI,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // Middleware
 app.use(cors());
