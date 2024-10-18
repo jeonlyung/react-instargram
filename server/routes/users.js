16b2b032a6e246bddcaa2a9cdbbb693ae070c58e
jeonlyung <bindongx2@naver.com>
2024-10-18 09:28:09 +0900

    User route 파일 추가

---
+++ server/routes/users.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const {User} = require("../models/User");
+
+//=================================
+//             User
+//=================================
+
+
+//유저 등록 API
+router.get("/register", (req,res) => {
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+       if(err) return res.json({success : false, err});
+
+       return res.status(200).json({
+           success : true
+       });
+    });
+
+});
